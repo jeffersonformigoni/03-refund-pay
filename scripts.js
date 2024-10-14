b062ce7ab807c6782ff12e894e0f7ef4466b5389
Jefferson Formigoni <jefferson.formigoni@khube.com.br>
2024-10-14 19:46:32 -0300

    Capturando o clique para remover um despesa

--- scripts.js
+++ scripts.js
@@ -162,3 +162,11 @@ function updateTotals() {
       alert("não foi possível atualizar os totais.")
     }
 }
+
+// Evento que captura o clique nos itens da lista.
+expenseList.addEventListener("click", function (event) {
+    // Verifica se o elemento clicado é o ícone de remover.
+    if (event.target.classList.contains("remove-icon")){
+      console.log(event)
+    }
+})
